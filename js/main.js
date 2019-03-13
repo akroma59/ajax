a0b03bc0c663414a8e659751ce4f0c808065582f
Vincent <vincentrichardeau@laposte.net>
2019-03-13 11:06:50 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
+  }
+};
+xhttp.open("GET", "client.php", true);
+xhttp.send(); 
+
